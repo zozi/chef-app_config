e0494def4a3d9c9f18d6a7c8d0f1fd07c480dc9c
Jeff Wallace <jeff@zozi.com>
2013-03-06 17:52:41 -0800

    Don't set app_name by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,3 @@
 default["app_config"]["default_environment"] = "default"
-default["app_config"]["app_name"] = "app"
 default["app_config"]["environment"] = "default"
 default["app_config"]["data_bag"] = "app_config"
